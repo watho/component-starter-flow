bae95c686b8eae7f5c901eea868500f657d5b1e1
Martin Vysny <mavi@vaadin.com>
2022-08-17 09:42:23 +0300

    Added MyComponent

---
+++ my-component/src/main/java/com/vaadin/starter/mycomponent/MyComponent.java
@@ -0,0 +1,10 @@
+package com.vaadin.starter.mycomponent;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+
+@Tag("my-component")
+@JsModule("./my-component/my-component.js")
+public class MyComponent extends Component {
+}
---
+++ my-component/src/main/resources/META-INF/resources/frontend/my-component/my-component.js
@@ -0,0 +1,11 @@
+import { html, LitElement } from 'lit';
+
+class MyComponent extends LitElement {
+    render() {
+        return html`
+      <h2>Hello</h2>
+    `;
+    }
+}
+
+window.customElements.define('my-component', MyComponent);
