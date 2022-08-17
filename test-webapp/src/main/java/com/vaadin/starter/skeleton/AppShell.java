1ffd7026c2a948735dc2484c7a53c6013af91a5c
Martin Vysny <mavi@vaadin.com>
2022-08-17 09:01:29 +0300

    introduced maven multi-module setup

---
+++ test-webapp/src/main/java/com/vaadin/starter/skeleton/AppShell.java
@@ -0,0 +1,8 @@
+package com.vaadin.starter.skeleton;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
+@PWA(name = "Test App For My-Component", shortName = "Test App")
+public class AppShell implements AppShellConfigurator {
+}
